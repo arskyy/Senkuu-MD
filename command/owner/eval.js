a52fd10e20c31167d31e50b80fd44cc887255460
X - Zeera ID <80798056+xzeera-id@users.noreply.github.com>
2022-08-16 21:11:08 +0700

    Gtau males

---
+++ command/owner/eval.js
@@ -0,0 +1,24 @@
+module.exports = {
+	name: "eval",
+	alias: [">>", ">","eval"],
+	category: "default",
+	noPrefix: true,
+	isOwner: true,
+	desc: "running javascript code via command can also test something code",
+	use: `">" <code javascript> with await and ">>" <code> live return or immediately show the result`,
+	query: `Masukan Parameter Code`,
+	async run({ msg, conn }, { q, map, args, Baileys, arg, prefix, response, chat }) {
+	  function _(stdout){
+	    msg.reply("```" + `root@Senkuu:\n${stdout}` + "```")
+	  }
+		let kode = msg.body.trim().split(/ +/)[0];
+		let teks;
+		try {
+			teks = await eval(`(async () => { ${kode == ">>" ? "return" : ""} ${q}})()`);
+		} catch (e) {
+			teks = e;
+		} finally {
+			await _(require("util").format(teks).trim());
+		}
+	},
+};
