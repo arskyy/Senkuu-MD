04915a3be4a078ebe14211e9632af3a2362f0738
X - Zeera ID <80798056+xzeera-id@users.noreply.github.com>
2022-08-17 13:20:36 +0800

    Create jadibot.js

---
+++ command/jadibot/jadibot.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "jadibot",
+  alias: ["jadibot"],
+  category: "main",
+  desc: "Numpang bot",
+  isPrivate: true,
+  async run({conn, msg},{map}){
+   // if(config.botNumber != config.botNumber) return msg.reply("_Tidak bisa membuat bot di dalam bot.._")
+    try {
+      require("../../jadibot").jadibot(msg, conn)
+    } catch(e) {
+      global.error(msg.command, e, msg)
+    }
+  }
+}
+
+
+
