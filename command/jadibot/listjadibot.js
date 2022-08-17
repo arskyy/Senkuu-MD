3e31b075ad82354dac1d2e3634700e1c69f28b1a
X - Zeera ID <80798056+xzeera-id@users.noreply.github.com>
2022-08-17 13:21:19 +0800

    Create listjadibot.js

---
+++ command/jadibot/listjadibot.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: "listjadibot",
+  alias: ["listjadibot","listbot"],
+  category: "main",
+  desc: "menampilkan user yang menumpang menjadi bot!!",
+  async run({conn, msg}){
+    try {
+      let user = [... new Set([...global.conns.filter(conn => conn.user).map(conn => conn.user)])]
+      te = "*List Jadibot*\n\n"
+      for(let i of user){
+        y = await conn.decodeJid(i.id)
+        te += " × User : @" + y.split("@")[0] + "\n"
+        te += " × Name : " + i.name + "\n\n"
+      }
+      user != "" ? await msg.reply(te,{withTag : true}) : await msg.reply("_*Tidak ada user yang menumpang..*_")
+    } catch (e){
+      global.error(msg.command, e, msg)
+    }
+  }
+}
