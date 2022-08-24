aa4af8b78c5a4631478a0485477c6cf1a8a571ed
X - Zeera ID <80798056+xzeera-id@users.noreply.github.com>
2022-08-24 21:26:21 +0700

    Update

--- main.js
+++ main.js
@@ -150,7 +150,7 @@ async function start(){
 	  }
 	  if (connection == "open") {
 	    console.log(chalk.yellow("Successfully connected to whatsapp"))
-	    conn.sendMessage(config.owner[0],{text: "*_Bot is now Online!*_"})
+	    conn.sendMessage(config.owner[0],{text: "_*Bot is now Online!*_"})
 	  }
 	  if (connection === "close") {
 			let reason = new Boom(lastDisconnect.error).output.statusCode;
