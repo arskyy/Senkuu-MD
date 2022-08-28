68c32112319cdac381a6418de2126ddf0b4ac13c
Rafi Adi Chandra <97791404+ChandraSans@users.noreply.github.com>
2022-08-28 08:17:31 +0700

    update menu.js

--- command/main/menu.js
+++ command/main/menu.js
@@ -46,18 +46,18 @@ module.exports = {
 	    category[cteg].push(info);
          }
         }
-                        teks = global.footer + " *[ Beta✓ ]*\n\n"
+                        teks = global.footer + " *[ Beta ✓ ]*\n\n"
 		  	teks += monospace(" ❏ Library : Baileys-MD") + "\n"
 		  	teks += monospace(" ❏ Author : " + "@" + config.owner[0].split("@")[0] )+ "\n"
 		  	teks += monospace(" ❏ Prefix : [ " + pref + " ]") + "\n\n"
-		  	teks += monospace(`Halo, @${sender.split("@")[0]} Here my Command`) +`\n\n`;
+		  	teks += monospace(`Hello, @${sender.split("@")[0]}!`) +`\n\n`;
 		  	teks += `*乂 ${q.toUpperCase()}*\n`
 		  	nganu = category[q]
 		  	if(nganu == undefined) throw "Category tidak ditemukan!!"
         for(let i of nganu){
           teks += monospace(` × ${pref + i.name} ${map.lockcmd.get(i.name) ? "❌" : ""}`) + "\n"
         }
-        teks += "\n*Bot Masih dalam tahap Perkembangan*"
+        teks += "\n*Bot masih dalam tahap Pengembangan!*"
         msg.reply(teks,{withTag: true})
       } else {
         for (let cmd of cmds){
@@ -73,15 +73,14 @@ module.exports = {
                 category[cteg].push(info);
           }
         }
-			menu = global.footer + " *[ Beta✓ ]*\n\n"
+			menu = global.footer + " *[ Beta ✓ ]*\n\n"
 			menu += monospace(" ❏ Library : Baileys-MD") + "\n"
 			menu += monospace(" ❏ Author : " + "@" + config.owner[0].split("@")[0] )+ "\n"
 			menu += monospace(" ❏ Prefix : [ " + pref + " ]") + "\n"
 			menu += monospace(" ❏ Date : " + date) + "\n"
 			menu += monospace(" ❏ Time : " + time) + "\n"
 		        menu += monospace(" ❏ Speed :  " + processTime(msg.messageTimestamp, moment()) + " Seccond") + "\n\n"
-		        menu += "*This Bot script is : https://github.com/xzeera-id/Senkuu-MD*\n_Tanda ❌ berarti Error atau Fitur sedang Dinonaktifkan oleh Owner!!_\n\n"
-			menu += monospace(`Halo, @${sender.split("@")[0]} Here my Command List`) +`\n\n`;
+			menu += monospace(`Hello, @${sender.split("@")[0]}!`) +`\n\n`;
 			const keys = Object.keys(category)
 			menu += "*乂 CATEGORY MENU*\n"
 			for(let o of keys){
@@ -92,7 +91,7 @@ module.exports = {
 			  menu += `*乂 ${key.toUpperCase()}*\n`
 			  menu += `${category[key].map((cmd) => monospace(` × ${cmd.options.noPrefix ? "" : pref}${cmd.name} ${map.lockcmd.get(cmd.name) ? "❌" : ""}`)).join("\n")} ` + "\n\n"
 			}
-			menu += `_Note : Ketik ${prefix}help <command> untuk melihat info command_`
+			menu += `Note : Ketik ${prefix}help <command> untuk melihat bantuan`
 			
 			const buttons = [
            { buttonId: `.owner`,buttonText:{displayText: 'Owner'}, type : 1},
@@ -101,7 +100,7 @@ module.exports = {
         const buttonMessage = {
            image: {url: "https://telegra.ph/file/642a95448d0d2d4750a37.jpg"},
            caption: menu,
-           footer: "Bot Masih dalam tahap Perkembangan",
+           footer: "Bot masih dalam tahap Pengembangan!",
            buttons: buttons,
            headerType: 1,
            withTag: true
