34d6a90142a190492a897d38cda526c9c7bdf07e
X - Zeera ID <80798056+xzeera-id@users.noreply.github.com>
2022-09-08 22:40:23 +0700

    Changed all

--- command/downloader/instagram.js
+++ command/downloader/instagram.js
@@ -1,4 +1,5 @@
 let { monospace, isUrl } = require('../../lib/function')
+let instagramGetUrl = require ("instagram-url-direct")
 
 module.exports = {
   name: "ig",
@@ -6,18 +7,17 @@ module.exports = {
 	category: "downloader",
 	use: "<url>",
 	async run({msg,conn},{q,args,map,cmdNya}){
-	  let { prefix } = map;
-    let { from, reply} = msg;
-    if(!q) throw "*[ INSTAGRAM DOWNLOADER ]*\n\nReply pesan ini dan kirim link url Instagram untuk Download.."
+          if(!q) throw "*[ INSTAGRAM DOWNLOADER ]*\n\nReply pesan ini dan kirim link url Instagram untuk Download.."
 	  try {
 	    await msg.reply(respon.wait)
-	    let igdl = await sc.instagram(q)
-	    if(/reel/.test(q)) return await conn.sendFile(msg.from, igdl.media[0].url,"", "*Done*", msg)
-	    ngontol = igdl.media.length > 1 ? true : false
-      if(ngontol) await msg.reply("Jumlah media lebih dari 1, media akan dikirim lewat private chat (PC)\nSilahkan cek chat dari bot><!")
-      for(let i of igdl.media) {
-        conn.sendFile(ngontol ? msg.sender : msg.from, i.url,"", "*Done*",msg)
-          }
+	    var result = await instagramGetUrl(q)
+            txt = "*乂 I N S T A - D O W N L O A D E R*"
+            if(/reel/.test(q)) return await conn.sendFile(msg.from, result.url_list,"", txt, msg)
+            chat = result.url_list.length > 1 ? true : false
+            if(chat) await msg.reply(`Jumlah media ${result.url_list.length}, media akan dikirim lewat private chat (PC)\nSilahkan cek chat dari bot><!`)
+            for(let i of result.url_list) {
+              conn.sendFile(chat ? msg.sender : msg.from, i,"", txt,msg)
+             }
 	  } catch (e){
 	    global.error(msg.command, e, msg)
 	  }
