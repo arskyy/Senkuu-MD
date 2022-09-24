06655a2bd2440246850935c4d2f3b68ad2f040f4
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-25 05:17:16 +0700

    Update linkgc.js

--- command/group/linkgc.js
+++ command/group/linkgc.js
@@ -8,13 +8,13 @@ module.exports = {
   async run({msg, conn},{args}) {
     let group = msg.from
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'Hanya bisa untuk id grup'
+    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'Only for group id'
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
     if (!('participants' in groupMetadata)) throw 'participants is not defined :('
     let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw 'Aku tidak ada di grup itu :('
-    if (!me.admin) throw 'Aku bukan admin T_T'
+    if (!me) throw 'I'm not in that group :('
+    if (!me.admin) throw 'I'm not an admin'
     msg.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group), {adReply : true})
   }
 }
