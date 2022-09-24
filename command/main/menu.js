2f053f118759a1b3b80f4266d72c0547978ef64f
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-25 04:52:28 +0700

    Update menu.js

--- command/main/menu.js
+++ command/main/menu.js
@@ -48,18 +48,18 @@ module.exports = {
 				  	category[cteg].push(info);
 		  		}
         }
-        teks = global.footer + " *[ Beta✓ ]*\n\n"
+        teks = global.footer + " *[ Recode Dari Senkuu-MD]*\n\n"
 		  	teks += monospace(" ❏ Library : Baileys-MD") + "\n"
 		  	teks += monospace(" ❏ Author : " + "@" + config.owner[0].split("@")[0] )+ "\n"
 		  	teks += monospace(" ❏ Prefix : [ " + pref + " ]") + "\n\n"
 		  	teks += monospace(`Halo, @${sender.split("@")[0]} Here my Command`) +`\n\n`;
 		  	teks += `*乂 ${q.toUpperCase()}*\n`
 		  	nganu = category[q]
-		  	if(nganu == undefined) throw "Category tidak ditemukan!!"
+		  	if(nganu == undefined) throw "Category not found!!"
         for(let i of nganu){
           teks += monospace(` × ${pref + i.name} ${map.lockcmd.get(i.name) ? "❌" : ""}`) + "\n"
         }
-        teks += "\n*Bot Masih dalam tahap Perkembangan*"
+        teks += "\n*Bot Still in Development stage*"
         msg.reply(teks,{withTag: true})
       } else {
         for (let cmd of cmds){
@@ -75,14 +75,14 @@ module.exports = {
 			  		category[cteg].push(info);
 			  	}
         }
-			menu = global.footer + " *[ Beta✓ ]*\n\n"
+			menu = global.footer + " *[ Recode Dari Senkuu-MD ]*\n\n"
 			menu += monospace(" ❏ Library : Baileys-MD") + "\n"
 			menu += monospace(" ❏ Author : " + "@" + config.owner[0].split("@")[0] )+ "\n"
 			menu += monospace(" ❏ Prefix : [ " + pref + " ]") + "\n"
 			menu += monospace(" ❏ Date : " + date) + "\n"
 			menu += monospace(" ❏ Time : " + time) + "\n"
 		  menu += monospace(" ❏ Speed :  " + processTime(msg.messageTimestamp, moment()) + " Seccond") + "\n\n"
-		  menu += "*This Bot script is : https://github.com/xzeera-id/Senkuu-MD*\n_Tanda ❌ berarti Error atau Fitur sedang Dinonaktifkan oleh Owner!!_\n\n"
+		  menu += "*This Bot script is : https://github.com/xzeera-id/Senkuu-MD*\n*Script Recode  : https://github.com/BOTCAHX/Senkuu-MD*\n_The sign ❌ means the Error or Feature is being Disabled by the Owner!!_\n\n"
 			menu += monospace(`Halo, @${sender.split("@")[0]} Here my Command List`) +`\n\n`;
 			const keys = Object.keys(category)
 			menu += "*乂 CATEGORY MENU*\n"
@@ -94,8 +94,8 @@ module.exports = {
 			  menu += `*乂 ${key.toUpperCase()}*\n`
 			  menu += `${category[key].map((cmd) => monospace(` × ${cmd.options.noPrefix ? "" : pref}${cmd.name} ${map.lockcmd.get(cmd.name) ? "❌" : ""}`)).join("\n")} ` + "\n\n"
 			}
-			menu += `*Thanks To:*\n• Senkuu\n•ZeraaID\n•Zynfinity\n\n`
-			menu += `_Note : Ketik ${prefix}help <command> untuk melihat info command_`
+			menu += `*Thanks To:*\n• Senkuu\n•ZeraaID\n•Zynfinity\n•BOTCAHX\n•XiaoSan\n\n`
+			menu += `_Note : Type ${prefix}help <command> to view command info_`
 			
 		/*	const buttons = [
            { buttonId: `.owner`,buttonText:{displayText: 'Owner'}, type : 1},
