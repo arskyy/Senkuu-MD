939a20bc39b1ac7d104638d08453060c9f550d87
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-25 05:00:28 +0700

    Update unlockcmd.js

--- command/owner/unlockcmd.js
+++ command/owner/unlockcmd.js
@@ -3,9 +3,9 @@ module.exports = {
 	alias: ["ulockcmd"],
 	category: "private",
 	isOwner: true,
-	desc: "membuka fitur ",
+	desc: "unlock features ",
 	use: `<name command>`,
-	query: `Masukan Parameter Nama Command`,
+	query: `Enter Parameter Name Command`,
 	async run({ msg, conn }, { q, map, args, arg }) {
 		var data = [...map.command.keys()];
 		[...map.command.values()]
@@ -14,8 +14,8 @@ module.exports = {
 			.replace(/ +/gi, ",")
 			.split(",")
 			.map((a) => data.push(a));
-		if (!data.includes(q)) throw "Command tidak ditemukan";
-		if (!map.lockcmd.has(q)) throw "Command ini belum di lock sebelumnya";
+		if (!data.includes(q)) throw "Command not found";
+		if (!map.lockcmd.has(q)) throw "This command has not been locked before";
 		map.lockcmd.delete(q);
 		await msg.reply(`Succes Membuka Lock Command "${q}"`);
 	},
